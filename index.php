823dc67c459407a2da7ae04aeb820adc8fcdb876
Muhammad Azizi Pujo Sakti <josaktii@gmail.com>
2021-04-08 20:08:48 +0800

    Dashboard admin
    Halaman admin dikerjakan

--- index.php
+++ index.php
@@ -11,26 +11,24 @@
 </head>
 
 <body class="d-flex h-100 text-center text-white bg-dark">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <header class="mb-auto">
-            <div>
+    <div class="container d-flex w-100 h-100 p-3 flex-column">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-auto">
+            <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-light text-decoration-none">
                 <h3 class="float-md-start mb-0">Cover</h3>
-                <nav class="nav nav-masthead justify-content-center float-md-end">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Contact</a>
-                </nav>
+            </a>
+
+            <div class="col-md-3 text-end">
+                <button type="button" class="btn btn-outline-primary me-2">Login</button>
+                <button type="button" class="btn btn-primary">Sign-up</button>
             </div>
         </header>
 
-        <main class="px-3">
-            <section>
-                <h1>Cover your page.</h1>
-                <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-                <p class="lead">
-                    <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-                </p>
-            </section>
+        <main class="px-3 cover-container mx-auto">
+            <h1>Cover your page.</h1>
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <p class="lead">
+                <a href="login.php" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+            </p>
         </main>
 
         <footer class="mt-auto text-white-50">
